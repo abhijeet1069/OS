5dc739d124083cc5959f46fae8ba3c7fb549ffb9
Abhijeet Kushwaha <46062413+abhijeet1069@users.noreply.github.com>
2021-03-29 13:18:48 +0530

    project template updated

--- process/demo.c
+++
@@ -1,6 +0,0 @@
-int Demo_length(char *str){
-    int i;
-    for (i = 0; str[i] != '\0'; ++i);
-
-    return i;
-}
--- process/main.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include "demo.h"
-
-int main(){
-    char *str = "Hello";
-    printf("Length of %s = %d\n",str, Demo_length(str));
-    return EXIT_SUCCESS;
-}
